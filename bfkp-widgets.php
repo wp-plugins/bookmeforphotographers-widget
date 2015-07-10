ebae5e9746362a1f9cd0cdc50801bb4fe0bc6ef9
bookmeforphotographers.com <bookmeforphotographers.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-10 23:09:26 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/bookmeforphotographers-widget/trunk@1196726 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bfkp-widgets.php
@@ -0,0 +1,119 @@
+<?php 
+/*
+Plugin Name: Bookmeforphotographers Widgets
+Plugin URI: http://www.bookmeforphotographers.com
+Description: Shortcode and Widget Creator
+Version: 1.0.0
+Author: Tiago G
+License: GPL2
+Copyright 2014 Tiago Gomes
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+ * Shortcode Syntax
+ * [ BKFP url='yourbookinglink' ]
+ * Attributes
+ * url - Your booking url
+ * ------------
+ * default values
+ * url=yourbookinglink
+ */
+
+if ( !class_exists('BKFP') )
+{
+	class BKFP
+	{
+
+		function __construct(){
+			add_shortcode('BKFP',array(&$this,'shortcode'));
+		
+		}
+		function shortcode($atts)
+		{
+            $base_url = "http://schedules.bookmeforphotographers.com/widgets/";
+			extract( shortcode_atts( array(
+					'url' => '',
+				), $atts ) );
+			return sprintf('<div id="div-bk"></div><script>(function(d, s, id) {var js, bks = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.src = "%s.js";bks.parentNode.insertBefore(js, bks);}(document, "script", "div-bk-js" ));</script>', $base_url.'button/'.base64_encode($url) );
+        }
+	}
+
+}	
+if ( !class_exists('BKFPWidget') )
+{
+
+	
+	class BKFPWidget {
+		function __construct()
+		{
+			$this->data = array(
+                'url' => '',
+			);
+			if ( !get_option('BKFPWidget') ) add_option('BKFPWidget',$this->data);
+			
+		}
+		function control()
+		{
+			$data = get_option('BKFPWidget');
+			extract($data);
+			echo '<div>';
+			
+			printf('<label><b>Schedule Link</b>:</label><br/><input required type="text" placeholder="url" name="BKFPWidgetUrl" value="%s" /></label>',$url);
+
+			echo '</div>';
+
+			if ( !empty($_POST['BKFPWidgetUrl']) ) $data['url'] = trim($_POST['BKFPWidgetUrl']);
+
+			update_option('BKFPWidget',$data);
+
+  		}
+		function widget($args)
+		{
+			extract(get_option('BKFPWidget'));
+    			echo $args['before_widget'];
+    			echo do_shortcode(sprintf('[BKFP url=%s ]',base64_encode($url)));
+    			echo $args['after_widget'];
+  		}
+		function register()
+		{
+		
+            wp_register_sidebar_widget(
+                'BKFP_widget_1',        // your unique widget id
+                'BKFP Widgets',          // widget name
+                array('BKFPWidget', 'widget'),  // callback function
+                array(                  // options
+                    'description' => 'Create BKFP Widgets'
+                )
+            );
+
+            wp_register_widget_control(
+                'BKFP_widget_1',        // your unique widget id
+                'BKFP Widgets',          // widget name
+                array('BKFPWidget', 'control'),  // callback function
+                array(                  // options
+                    'description' => 'Create BKFP Widgets'
+                )
+            );
+    		
+  		}
+
+    }
+	
+}
+new BKFP();
+new BKFPWidget();
+add_action("widgets_init", array('BKFPWidget', 'register'));
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Bookmeforphotographers Widgets ===
+Contributors: Tiago G
+Version: 1.0.0
+Create Booking buttons either by using shortcode or use WordPress widgets to place follow button on your blog sidebars.
+
+== Description ==
+Add the Booking Button to your website to increase engagement and create a lasting connection with your audience.
+
+<strong>Using as a Widget</strong>
+<p>When "Bookmeforphotographers Widgets" is activated a widget is also created with title 'Bookmeforphotographers Widgets' you can use that widget to place follow button on blog sidebars.</p>
+
+<strong>Shortcode Syntax</strong>
+<code>[BKFP url='booking url']</code>
+
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+
+== Frequently Asked Questions ==
+**ShortCode Syntax and guide**
+Shortcode Syntax
+[BKFP url='booking url']
+Attributes
+{url} - Your booking url.
+
+------------
+**Default values**
+url=hxxp:schedules.bookmeoforphotographers.com/*
+
